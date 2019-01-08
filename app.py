935f698ff5d4f5c7cd93e83f67a12197baaa3fb1
Damilare <justdamilare@gmail.com>
2019-01-08 13:39:31 +0100

    Added manage.py and models.py Updated app.py

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
-import os
 from flask import Flask
-
+from flask.ext.sqlalchemy import SQLAlchemy
+import os
+from models import Result
 
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
 
 @app.route('/')
@@ -14,6 +17,7 @@ def hello():
 @app.route('/<name>')
 def hello_name(name):
     return "Hello {}!".format(name)
+
 
 if __name__ == '__main__':
     app.run()
